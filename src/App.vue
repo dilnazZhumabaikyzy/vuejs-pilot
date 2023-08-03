5a6a2e61c0a5467615f27ca0c4e38049291d3984
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2023-08-04 02:54:17 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    Pabotaet!
+    <button v-on:click="addLike">Like</button>
+    <button @click="addDislike">Dislike</button>
+    <div>Likes: <strong>{{ likes }} </strong></div>
+    <div>Dislikes: <strong>{{ dislikes }}</strong></div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            likes: 0,
+            dislikes: 5
+        }
+    },
+    methods: {
+        addLike(){
+            this.likes += 1;
+        },
+        addDislike(){
+            this.dislikes += 1;
+        },
+    }
+}
+</script>
